2b037ccce4ac743ccc135d2757157a5b06d9bdd7
michaelmg32 <michaelgungun45@gmail.com>
2026-07-16 00:39:57 +0700

    asd

--- server.js
+++ server.js
@@ -111,6 +111,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'pelayanan_cuci_ac',
+  dateStrings: true,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
